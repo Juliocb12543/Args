5cdb1708bca134554c1654f228f57b74415013aa
Julio Barrientos <113930475+Juliocb12543@users.noreply.github.com>
2022-11-15 17:56:50 +0000

    challenge problems

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-#args means I can use as many arguments as possible
-def sum(*args):
-  total = 0
-  for arg in args:
-    total += arg
-  return total
+# #args means I can use as many arguments as possible
+# def sum(*args):
+#   total = 0
+#   for arg in args:
+#     total += arg
+#   return total
 
-print(sum(2,3,1,6))
+# print(sum(2,3,1,6))
 
 
 
-#kwargs means using key values(limitless)
+# #kwargs means using key values(limitless)
+# #a step beyond args
+# #keys and values represented **
+# #dictionary = key, value; items are keys and values together
+# def a_sum(**kwargs):
+#   total = 0
+#   for key, value in kwargs.items():
+#     print(f"{key} = {value}")
+#     total += value
+#   return total
+
+# print(a_sum(x=3,y=5, z=22))
